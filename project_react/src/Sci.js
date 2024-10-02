501c1fbc0bf30ca9c26e223048adaa8a09175a07
Selerek <90503520+Selerek@users.noreply.github.com>
2024-10-02 09:27:54 +0200

    button input eventhandler
    lekcja1 02.10.2024

--- project_react/src/Sci.js
+++ project_react/src/Sci.js
@@ -1,7 +1,21 @@
+import { useRef } from "react"
+
 function Sci(){
+    const inputRef = useRef()
+    const buttonhandler = () => {
+       console.log("wowowow")
+       console.log(inputRef.current.value)
+        
+    }
+    const inputhandler = (event) => {
+        //console.log(event.target.value)
+        
+          
+    }
     return (
         <div>
-            sci
+            <input onClick={buttonhandler} type="button" value={123123}></input>
+            <input ref = {inputRef} onChange={inputhandler} type="text" ></input>
         </div>
     )
 }
