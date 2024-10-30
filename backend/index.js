d86e7a045c636b340bbb494aec62d2bbf9d04476
Selerek <90503520+Selerek@users.noreply.github.com>
2024-10-30 08:34:06 +0100

    backend creation

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+
+const application = express()
+
+application.get('/', (request, response) => response.send("<div><h1>hello world</h1></div>"))
+application.get('/abc', (request, response) => {
+    response.status(500)
+    response.send("<div><h1>chong</h1></div>")
+})
+application.post('/abc', (request, response) => {
+    response.status(500)
+    response.send("<div><h1>abc</h1></div>")
+})
+application.put('/abc', (request, response) => {
+    response.status(404)
+    response.send("<div><h1>putter</h1></div>")
+})
+//sci/4c/abc
+const sci_router = express.Router()
+sci_router.get('/', (request, response) => {
+    response.send("sci")
+})
+sci_router.get('/json', (request, response) => {
+    const data = [{
+        key1: "text",
+        key2: 123,
+    }]
+    response.json(data)
+})
+application.use("/sci/4c/abc", sci_router)
+
+
+
+
+
+
+
+
+
+
+
+
+application.listen(8000, () => console.log("server start") )
