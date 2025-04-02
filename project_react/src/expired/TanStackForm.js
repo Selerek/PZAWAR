92d6ac3126dff85e2a9c22afc131b68cb4129e1f
Selerek <90503520+Selerek@users.noreply.github.com>
2025-04-02 19:48:28 +0200

    zaczynamy jazde
    jedziemy zadanie z tanstackiem

--- project_react/src/expired/TanStackForm.js
+++ project_react/src/expired/TanStackForm.js
@@ -7,11 +7,11 @@ import bcrypt from 'bcryptjs';
 export default function App() {
   const form = useForm({
     defaultValues: {
+      name: '',
       email: '',
       password: '',
       //confirm_password: '',
-      gender: '',
-      school: '',
+
     },
     onSubmit: async (values) => {
       const hashedPassword = await bcrypt.hash(values.value.password, 10);
@@ -38,6 +38,36 @@ export default function App() {
           form.handleSubmit();
         }}
       >
+        <div className="mb-3">
+          <form.Field
+            name="name"
+            validators={{
+              onChange: ({value}) => {
+                if(!value) {
+                  return 'Name is required';
+                }
+                else {
+                  return undefined;
+                }
+              }
+            }}
+            children={(field) => (
+              <div>
+                <label className="form-label">Name</label>
+                <input
+                  className="form-control"
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {field.state.meta.errors ? (
+                  <em role="alert">{field.state.meta.errors.join(', ')}</em>
+                ) : null}
+              </div>
+            )}
+          />
+        </div>
         <div className="mb-3">
           <form.Field
             name="email"
@@ -99,118 +129,7 @@ export default function App() {
             )}
           />
         </div>
-        <div className="mb-3">
-          <form.Field
-            name="confirm_password"
-            validators={{
-              onChange: ({value}) => {
-                if(!value) {
-                  return 'Email is required';
-                }
-                else {
-                  return undefined;
-                }
-              }
-            }}
-            children={(field) => (
-              <div>
-                <label className="form-label">Confirm Password</label>
-                <input
-                  className="form-control"
-                  name={field.name}
-                  value={field.state.value}
-                  type="password"
-                  onBlur={field.handleBlur}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                />
-                {field.state.meta.errors ? (
-                  <em role="alert">{field.state.meta.errors.join(', ')}</em>
-                ) : null}
-              </div>
-            )}
-          />
-        </div>
-        <div className="mb-3">
-          <form.Field
-            name="gender"
-            validators={{
-              onChange: ({value}) => {
-                if(!value) {
-                  return 'Email is required';
-                }
-                else {
-                  return undefined;
-                }
-              }
-            }}
-            children={(field) => (
-              <div>
-                <label className="form-label">Gender</label>
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name={field.name}
-                    value="man"
-                    checked={field.state.value === 'man'}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                  <label className="form-check-label">Man</label>
-                </div>
-                <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name={field.name}
-                    value="woman"
-                    checked={field.state.value === 'woman'}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                  <label className="form-check-label">Woman</label>
-                </div>
-                {field.state.meta.errors ? (
-                  <em role="alert">{field.state.meta.errors.join(', ')}</em>
-                ) : null}
-              </div>
-            )}
-          />
-        </div>
-        <div className="mb-3">
-          <form.Field
-            name="school"
-            validators={{
-              onChange: ({value}) => {
-                if(!value) {
-                  return 'Email is required';
-                }
-                else {
-                  return undefined;
-                }
-              }
-            }}
-            children={(field) => (
-              <div>
-                <label className="form-label">School</label>
-                <select
-                  className="form-select"
-                  name={field.name}
-                  value={field.state.value}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                >
-                  <option value="">Choose your school</option>
-                  <option value="sci">SCI</option>
-                  <option value="tme">TME</option>
-                  <option value="twojstary">Twojstary</option>
-                  <option value="teb">TEB</option>
-                  <option value="toiz">TOIZ</option>
-                </select>
-                {field.state.meta.errors ? (
-                  <em role="alert">{field.state.meta.errors.join(', ')}</em>
-                ) : null}
-              </div>
-            )}
-          />
-        </div>
+        
         
         <button className="btn btn-primary" type="submit">
           Submit
